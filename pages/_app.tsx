4faee75cecf16531288692d38496ff0247e5c6fb
Mikhail Sergeevich <mik2288mik@gmail.com>
2026-07-19 00:54:53 +0300

    feat: load card background styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import '../styles/stickers.css';
+import '../styles/cardBackgrounds.css';
 import { DoodleDefs } from '../components/doodle/DoodleDefs';
 
 export default function App({ Component, pageProps }: AppProps) {
