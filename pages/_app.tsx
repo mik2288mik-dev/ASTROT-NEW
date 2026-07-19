94e32f0640054ab6e17d83a20ff956eafd59e97a
Mikhail Sergeevich <mik2288mik@gmail.com>
2026-07-20 00:33:25 +0300

    fix: load scroll stability overrides last

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import '../styles/homeMvpLayout.css';
 import '../styles/typographyManrope.css';
 import '../styles/homeContentHierarchy.css';
 import '../styles/readingBackgrounds.css';
+import '../styles/homeScrollStability.css';
 import { DoodleDefs } from '../components/doodle/DoodleDefs';
 
 export default function App({ Component, pageProps }: AppProps) {
