3fe26b4d595f70b37064bcf090bd148cb6338377
Mikhail Sergeevich <mik2288mik@gmail.com>
2026-09-08 03:14:48 +0300

    feat(ui): load NEBO matrix parity styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -27,6 +27,7 @@ import '../styles/natalMeaningMap.css';
 import '../styles/publicSiteDocument.css';
 import '../styles/neboV2.css';
 import '../styles/neboV2Parity.css';
+import '../styles/neboV2Matrix.css';
 import { DoodleDefs } from '../components/doodle/DoodleDefs';
 import { PublicAnalytics } from '../components/public-site/PublicAnalytics';
 import { installRuntimeDiagnostics } from '../lib/runtimeDiagnostics';
